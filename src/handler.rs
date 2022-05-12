c4f9db78438a37cea6346f34d148586b2bb6ba51
ChousX <gmanenergy@gmail.com>
2022-05-12 14:08:24 -0400

    Well I got it connect but its not detecting any messages

---
+++ src/handler.rs
@@ -0,0 +1,57 @@
+use rand::prelude::*;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+    utils::MessageBuilder,
+};
+
+const HELP: &str =
+    "Why are we here?\nTO SMOKE MEATS!!!\nCommand List:\nHelp: !help\n Coin Flip: !flip";
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let subject: &str = &msg.content;
+        if subject == "!help" {
+            let response = MessageBuilder::new().push(HELP).build();
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error Sending message: {:?}", why);
+            }
+        }
+
+        if subject == "!flip" {
+            let result = if thread_rng().gen_bool(0.5) {
+                "Heads".to_string()
+            } else {
+                "Tails".to_string()
+            };
+
+            let response = MessageBuilder::new()
+                .push_bold_safe(&msg.author.name)
+                .push(result)
+                .build();
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error Sending message: {:?}", why);
+            }
+        }
+
+        if subject == "!d20" {
+            let result = thread_rng().gen_range(1..21);
+            let response = MessageBuilder::new()
+                .push_bold_safe(&msg.author.name)
+                .push(result.to_string())
+                .build();
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error Sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use serenity::prelude::*;
+mod handler;
+use handler::Handler;
+
+fn get_token() -> String {
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    let mut file = File::open(".token").expect("Can't open .token");
+    let mut token = String::new();
+    file.read_to_string(&mut token).expect("Cant read .token");
+    token
+}
+
+#[tokio::main]
+async fn main() {
+    let token = get_token();
+    let mut client = Client::builder(&token, GatewayIntents::default())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
