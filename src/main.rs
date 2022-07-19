9e968ca5fe9f4f1925a77bcbc7a9f591e6a3b8bd
ChousX <gmanenergy@gmail.com>
2022-07-18 21:14:51 -0400

    cleaned up and ready for more commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::env;
+use chrono::{Utc, DateTime, TimeZone, Local};
+use rand::{thread_rng, Rng};
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
 use serenity::prelude::*;
-mod handler;
-use handler::Handler;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
+use serenity::utils::MessageBuilder;
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 
 fn get_token() -> String {
     use std::fs::File;
@@ -15,15 +31,66 @@ fn get_token() -> String {
 #[tokio::main]
 async fn main() {
     let token = get_token();
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!")) // set the bot's prefix to "!"
+        .group(&GENERAL_GROUP);
+
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
+        .framework(framework)
         .await
         .expect("Error creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+#[group]
+#[commands(help, flip, d20, its5)]
+pub struct General;
+
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+    let subject: &str = &msg.content;
+    let response = MessageBuilder::new()
+    .push("Why are we here?\n")
+    .push_bold_line_safe("TO SMOKE MEATS!!!")
+    .push_bold_line_safe("Command List:")
+    .push_bold_safe("!help")
+    .push(": brings you here and lists of all the commands\n")
+    .push_bold_safe("!flip")
+    .push(": flips a coin\n")
+    .push_bold_safe("!d20")
+    .push(": rolls a d20\n")
+    .build();
+    msg.reply(ctx, response).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn flip(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut response = MessageBuilder::new();
+    response.push_bold_safe(&msg.author.name);
+    response.push(if thread_rng().gen_bool(0.5) { " Heads"} else { " Tails"});
+    msg.reply(ctx, response.build()).await?;
+    Ok(())
+}
+
+#[command]
+async fn d20(ctx: &Context, msg: &Message) -> CommandResult{
+    let result = thread_rng().gen_range(1..=20);
+            let mut response = MessageBuilder::new();
+            response
+                .push_bold_safe(&msg.author.name)
+                .push(" ")
+                .push(result.to_string());
+    msg.reply(ctx, response.build()).await?;
+    
+    Ok(())
 }
